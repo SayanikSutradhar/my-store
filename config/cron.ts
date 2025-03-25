cc44e0e7b21bad5e44395fb26419c2b77dd07aa2
SayanikSutradhar <sayaniksutradhar2110@gmail.com>
2025-03-25 19:10:43 +0530

    New File

---
+++ config/cron.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const cronTasks = {
+  '*/10 * * * * *': {
+    task: async () => {
+      console.log(`Current Date & Time: ${new Date().toISOString()}`);
+
+      try {
+        await axios.get('https://my-store-strapi.onrender.com/health');
+        console.log('Pinged server to keep it alive.');
+      } catch (error) {
+        console.error('Ping failed:', error.message);
+      }
+    },
+    options: {
+      tz: 'UTC', // Optional: Set timezone
+    },
+  },
+};
+
+export default cronTasks;
